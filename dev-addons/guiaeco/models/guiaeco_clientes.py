3c6005b07e5e91d50e3c7823779f712b53d72247
juanjocop <juanjocop@gmail.com>
2018-06-28 17:15:57 +0200

    Iniciamos el model de la guia

---
+++ dev-addons/guiaeco/models/guiaeco_clientes.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+# class dev-addons/library(models.Model):
+#     _name = 'dev-addons/library.dev-addons/library'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
+
+class GuiaecoClientes(models.Model):
+    _name = 'guiaeco.clientes'
+
+    idcliente = fields.Integer(string='Id Cliente')
+    activo = fields.Boolean(string='Activo?')
+    imagenPlaca = fields.Char(string='Imagen placa cliente')
+    cabecera = fields.Char(string='Cabecera cliente')
+    especialidad = fields.Char(string='Especialidad')
+    mapaG = fields.Char(string='Enlace Mapa')
+    calidadPresentacion = fields.Char(string='Como valora la presentación del producto?')
+    calidadTrato = fields.Char(string='Trato recibido por parte de los agentes')
+    calidadRecomienda = fields.Boolean(string='Lo recomendaría a otras personas?')
+    calidadBeneficios = fields.Integer(string='Beneficios importantes para usted y su negocio')
+    calidadValora = fields.Integer(string='Como valora eOZONE y ecoWORLD?')
