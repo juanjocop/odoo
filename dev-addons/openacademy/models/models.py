a8d2b86b580e2fe890815cd80a072a2fd35862aa
juanjocop <juanjocop@gmail.com>
2018-07-09 19:32:00 +0200

    Actualizamos modulo openacademy para aprender a usar guia, parte relacional completa

--- dev-addons/openacademy/models/models.py
+++ dev-addons/openacademy/models/models.py
@@ -2,14 +2,29 @@
 
 from odoo import models, fields, api
 
-class openacademy(models.Model):
-    _name = 'openacademy.openacademy'
+class Course(models.Model):
+    _name = 'openacademy.curso'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    name = fields.Char(string="Título", required=True)
+#    value = fields.Integer()
+#    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text(string="Descripción")
+
+    responsable_id =fields.Many2one("res.users", ondelete="set null", string="Responsable", index=True)
+    sesion_id = fields.One2many("openacademy.sesion", "curso_id", string="Sesiones")
 #
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
+#    @api.depends('value')
+#    def _value_pc(self):
+#        self.value2 = float(self.value) / 100
+
+class Sesion(models.Model):
+    _name = 'openacademy.sesion'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duración en días")
+    seats = fields.Integer(string="Número de asientos")
+
+    instructor_id = fields.Many2one("res.partner", string="Instructor")
+    curso_id = fields.Many2one("openacademy.curso", ondelete="cascade", string="Curso", required=True)
+    asistente_id = fields.Many2many("res.partner", string="Asistente")
