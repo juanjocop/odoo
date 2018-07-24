d84f043fde72c5fe7002c3e4f05948a086ac0738
juanjocop <juanjocop@gmail.com>
2018-07-24 18:33:08 +0200

    ya tenemos testimonios en administración

--- dev-addons/guiaecoworld/models/__init__.py
+++ dev-addons/guiaecoworld/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import guia_ecoworld
+from . import testimonio
--- dev-addons/guiaecoworld/models/testimonio.py
+++ dev-addons/guiaecoworld/models/testimonio.py
@@ -3,8 +3,14 @@
 from odoo import models, fields, api
 
 class testimonio(models.Model):
-    _name = 'testimonios'
+    _name = 'guiaeco.testimonios'
     _inherits = {'res.partner': 'partner_id'}
 
-    partner_id = fields.Many2one('res.partner', domain=[('is_company', '=', False)])
-    testimonio = fields.Char(string="Testimonio")
+    partner_id = fields.Many2one('res.partner', domain=[('is_company', '=', False)], required=True, ondelete="restrict")
+    activo = fields.Boolean(string="Testimonio Activado")
+    testimonio = fields.Text(string="Testimonio")
+    calidadPresentacion = fields.Selection([(1,'Bien'), (2,'Muy Bien'), (3,'Excelente')],string="Presentación")
+    calidadTrato = fields.Selection([(1,'Correcto'), (2,'Agradable'), (3,'Excepcional')],string="Trato recibido")
+    calidadRecomienda = fields.Boolean(string="Nos Recomienda?")
+    calidadBeneficios = fields.Char(string="Beneficios")
+    calidadValora = fields.Integer(string="Valoración General")
